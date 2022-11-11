02ba350a602f4cc56e70b37ca63e146d84d1d177
Silverfox <77423595+silverfoxpt@users.noreply.github.com>
2022-11-11 20:33:34 +0700

    Push 8h33 11/11/22

---
+++ CODE_PROBLEM/TRAFFIC_LIGHT.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <map>
+#include <set>
+#include <cstdio>
+#include <iomanip>
+#include <cctype>
+#include <utility>
+#include <cmath>
+#include <climits>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+    int t; cin >> t;
+}
